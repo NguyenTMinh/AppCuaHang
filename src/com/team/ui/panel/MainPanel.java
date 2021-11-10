817d196ea9b8ecd07d4d6f2daec93fdba55bc563
VietHoan <Lehoan04072001@gmail.com>
2021-11-10 17:51:51 +0700

    1

--- src/com/team/ui/panel/MainPanel.java
+++ src/com/team/ui/panel/MainPanel.java
@@ -1,28 +1,26 @@
 package com.team.ui.panel;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.LayoutManager;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SpringLayout;
 
-import com.team.logic.SanPhamCuaHang;
 import com.team.ui.ActionClick;
 
 
 public class MainPanel extends BasePanel implements ActionClick{
 	private PanelDangNhap panelDangNhap;
-	private PanelPhienCH panelPhienCH;
-	private PanelListSP panelListSP;
-	private PanelAddSp panelAddSp;
-
 
 	public MainPanel() {
+
 	}
 	
 	@Override
 	public void initUI() {
-
 		setBackground(Color.WHITE);
 		setLayout(new CardLayout());
 	}
@@ -35,27 +33,12 @@ public class MainPanel extends BasePanel implements ActionClick{
 	@Override
 	public void addComp() {
 		panelDangNhap = new PanelDangNhap();
-		panelDangNhap.setActionClick(this);
 		add(panelDangNhap);
-
-		panelPhienCH = new PanelPhienCH();
-		panelPhienCH.setActionClick_ListSP(this);
-		add(panelPhienCH);
-
-		panelListSP = new PanelListSP();
-		panelListSP.setActionClick(this);
-		add(panelListSP);
-
-		panelAddSp = new PanelAddSp();
-		panelAddSp.setActionClick(this);
-		add(panelAddSp);
-
 	}
 
 	@Override
 	public void actionClick() {
-		panelPhienCH.setVisible(true);
-		panelDangNhap.setVisible(false);
+		
 	}
 
 	@Override
@@ -70,36 +53,5 @@ public class MainPanel extends BasePanel implements ActionClick{
 		
 	}
 
-	@Override
-	public void goBacktoLogin() {
-		panelDangNhap.setVisible(true);
-		panelPhienCH.setVisible(false);
-	}
-
-	@Override
-	public void goBacktoPhienCH() {
-		panelPhienCH.setVisible(true);
-		panelListSP.setVisible(false);
-	}
-
-	@Override
-	public void acctionShowSP() {
-		panelListSP.setModelList(panelAddSp.getModel_SpCH());
-		panelListSP.setVisible(true);
-		panelPhienCH.setVisible(false);
-		panelAddSp.setVisible(false);
-	}
-
-	@Override
-	public void addSp() {
-		panelListSP.setVisible(false);
-		panelAddSp.setVisible(true);
-	}
-
-
-	@Override
-	public DefaultListModel<SanPhamCuaHang> getModel_SpCH() {
-		return panelAddSp.getModel_SpCH();
-	}
-
+	
 }
--- src/com/team/ui/panel/PanelDangNhap.java
+++ src/com/team/ui/panel/PanelDangNhap.java
@@ -1,30 +1,15 @@
 package com.team.ui.panel;
 
-import com.team.ui.ActionClick;
-import com.team.ui.GUI;
-
 import javax.swing.*;
 import java.awt.*;
-import java.util.Objects;
 
 public class PanelDangNhap extends BasePanel{
-    private JLabel jLabel_Title;
     private JLabel jLabel_Login;
-    private JLabel jLabel_Acc;
-    private JLabel jLabel_Pass;
-
-    private JTextField jTextField_Acc;
-    private JPasswordField jPasswordField_Pass;
-
-    private JButton jButton_Login;
-    private JButton jButton_Return;
-
-    private ActionClick actionClick;
 
     @Override
     public void initUI() {
         setLayout(null);
-        setBackground(Color.white);
+        setBackground(Color.green);
         setVisible(true);
     }
 
@@ -35,55 +20,8 @@ public class PanelDangNhap extends BasePanel{
 
     @Override
     public void addComp() {
-        Font big_font = new Font("Tahoma",Font.PLAIN,60);
-        Font font = new Font("Tahoma",Font.PLAIN,30);
-        Font small_font = new Font("Tahoma",Font.PLAIN,20);
-
-        jLabel_Title = creatLabel("STORE",290,50,big_font,Color.BLACK,null);
-        jLabel_Title.setSize(220,60);
-        add(jLabel_Title);
-
-        jLabel_Login = creatLabel("Đăng nhập",322,150,font,Color.black,null);
+        Font font = new Font("Tahoma",Font.PLAIN,60);
+        jLabel_Login = creatLabel("STORES",300,50,font,Color.BLACK,Color.white);
         add(jLabel_Login);
-
-        jTextField_Acc = createTextField(250,300,320,small_font,Color.black);
-        add(jTextField_Acc);
-        jLabel_Acc = creatLabel("Tài Khoản",138,300,small_font,Color.black,null);
-        add(jLabel_Acc);
-
-        jPasswordField_Pass = new JPasswordField();
-        jPasswordField_Pass.setFont(small_font);
-        jPasswordField_Pass.setSize(320,jTextField_Acc.getHeight());
-        jPasswordField_Pass.setLocation(250,360);
-        add(jPasswordField_Pass);
-        jLabel_Pass = creatLabel("Mật Khẩu",140,360,small_font,Color.black,null);
-        add(jLabel_Pass);
-
-        jButton_Login = creatButton("Xác nhận",315,480,small_font,Color.black,"button_login");
-        jButton_Login.setSize(160,50);
-        jButton_Login.setBackground(Color.decode("#97D7D3"));
-        add(jButton_Login);
-
-        jButton_Return = creatButton("Quay lại ",10,10,new Font("Tahoma",Font.PLAIN,16),Color.black,"button_return");
-        jButton_Return.setBackground(null);
-        jButton_Return.setBorder(null);
-        jButton_Return.setSize(90,30);
-        add(jButton_Return);
-    }
-
-    @Override
-    protected void handleClick(String name) {
-        if(name.equals("button_login")){
-            actionClick.actionClick();
-        }
-        else System.out.println("Sai");
-    }
-
-    public ActionClick getActionClick() {
-        return actionClick;
-    }
-
-    public void setActionClick(ActionClick actionClick) {
-        this.actionClick = actionClick;
     }
 }
